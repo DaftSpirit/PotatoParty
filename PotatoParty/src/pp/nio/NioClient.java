628efe896e1822c7df2d5ddee592573f61c60837
Daft_Spirit <joris.rat@gmail.com>
2015-12-29 17:06:14 +0100

    azeaze

--- PotatoParty/src/pp/nio/NioClient.java
+++ PotatoParty/src/pp/nio/NioClient.java
@@ -29,10 +29,11 @@ public class NioClient implements Runnable {
 	// Maps a SocketChannel to a list of ByteBuffer instances
 	@SuppressWarnings("rawtypes")
 	private Map<SocketChannel, List> pendingData = new HashMap<SocketChannel, List>();
-	
+
 	// Maps a SocketChannel to a RspHandler
-	private Map<SocketChannel, RspHandler> rspHandlers = Collections.synchronizedMap(new HashMap<SocketChannel, RspHandler>());
-	
+	private Map<SocketChannel, RspHandler> rspHandlers = Collections
+			.synchronizedMap(new HashMap<SocketChannel, RspHandler>());
+
 	public NioClient(InetAddress hostAddress, int port) throws IOException {
 		this.hostAddress = hostAddress;
 		this.port = port;
@@ -42,14 +43,15 @@ public class NioClient implements Runnable {
 	public void send(byte[] data, RspHandler handler) throws IOException {
 		// Start a new connection
 		SocketChannel socket = this.initiateConnection();
-		
+
 		// Register the response handler
 		this.rspHandlers.put(socket, handler);
-		
+
 		// And queue the data we want written
 		synchronized (this.pendingData) {
 			@SuppressWarnings("unchecked")
-			List<ByteBuffer> queue = (List<ByteBuffer>) this.pendingData.get(socket);
+			List<ByteBuffer> queue = (List<ByteBuffer>) this.pendingData
+					.get(socket);
 			if (queue == null) {
 				queue = new ArrayList<ByteBuffer>();
 				this.pendingData.put(socket, queue);
@@ -57,7 +59,8 @@ public class NioClient implements Runnable {
 			queue.add(ByteBuffer.wrap(data));
 		}
 
-		// Finally, wake up our selecting thread so it can make the required changes
+		// Finally, wake up our selecting thread so it can make the required
+		// changes
 		this.selector.wakeup();
 	}
 
@@ -66,12 +69,14 @@ public class NioClient implements Runnable {
 			try {
 				// Process any pending changes
 				synchronized (this.pendingChanges) {
-					Iterator<ChangeRequest> changes = this.pendingChanges.iterator();
+					Iterator<ChangeRequest> changes = this.pendingChanges
+							.iterator();
 					while (changes.hasNext()) {
 						ChangeRequest change = (ChangeRequest) changes.next();
 						switch (change.type) {
 						case ChangeRequest.CHANGEOPS:
-							SelectionKey key = change.socket.keyFor(this.selector);
+							SelectionKey key = change.socket
+									.keyFor(this.selector);
 							key.interestOps(change.ops);
 							break;
 						case ChangeRequest.REGISTER:
@@ -86,7 +91,8 @@ public class NioClient implements Runnable {
 				this.selector.select();
 
 				// Iterate over the set of keys for which events are available
-				Iterator<?> selectedKeys = this.selector.selectedKeys().iterator();
+				Iterator<?> selectedKeys = this.selector.selectedKeys()
+						.iterator();
 				while (selectedKeys.hasNext()) {
 					SelectionKey key = (SelectionKey) selectedKeys.next();
 					selectedKeys.remove();
@@ -140,21 +146,22 @@ public class NioClient implements Runnable {
 		this.handleResponse(socketChannel, this.readBuffer.array(), numRead);
 	}
 
-	private void handleResponse(SocketChannel socketChannel, byte[] data, int numRead) throws IOException {
+	private void handleResponse(SocketChannel socketChannel, byte[] data,
+			int numRead) throws IOException {
 		// Make a correctly sized copy of the data before handing it
 		// to the client
 		byte[] rspData = new byte[numRead];
 		System.arraycopy(data, 0, rspData, 0, numRead);
-		
+
 		// Look up the handler for this channel
 		RspHandler handler = (RspHandler) this.rspHandlers.get(socketChannel);
-		
+
 		// And pass the response to it
-		if (handler.handleResponse(rspData)) {
-			// The handler has seen enough, close the connection
-			//socketChannel.close();
-			//socketChannel.keyFor(this.selector).cancel();
-		}
+		handler.handleResponse(rspData);
+		// The handler has seen enough, close the connection
+		// socketChannel.close();
+		// socketChannel.keyFor(this.selector).cancel();
+
 	}
 
 	private void write(SelectionKey key) throws IOException {
@@ -185,7 +192,7 @@ public class NioClient implements Runnable {
 
 	private void finishConnection(SelectionKey key) throws IOException {
 		SocketChannel socketChannel = (SocketChannel) key.channel();
-	
+
 		// Finish the connection. If the connection operation failed
 		// this will raise an IOException.
 		try {
@@ -196,7 +203,7 @@ public class NioClient implements Runnable {
 			key.cancel();
 			return;
 		}
-	
+
 		// Register an interest in writing on this channel
 		key.interestOps(SelectionKey.OP_WRITE);
 	}
@@ -205,18 +212,20 @@ public class NioClient implements Runnable {
 		// Create a non-blocking socket channel
 		SocketChannel socketChannel = SocketChannel.open();
 		socketChannel.configureBlocking(false);
-	
+
 		// Kick off connection establishment
-		socketChannel.connect(new InetSocketAddress(this.hostAddress, this.port));
-	
-		// Queue a channel registration since the caller is not the 
+		socketChannel
+				.connect(new InetSocketAddress(this.hostAddress, this.port));
+
+		// Queue a channel registration since the caller is not the
 		// selecting thread. As part of the registration we'll register
 		// an interest in connection events. These are raised when a channel
 		// is ready to complete connection establishment.
-		synchronized(this.pendingChanges) {
-			this.pendingChanges.add(new ChangeRequest(socketChannel, ChangeRequest.REGISTER, SelectionKey.OP_CONNECT));
+		synchronized (this.pendingChanges) {
+			this.pendingChanges.add(new ChangeRequest(socketChannel,
+					ChangeRequest.REGISTER, SelectionKey.OP_CONNECT));
 		}
-		
+
 		return socketChannel;
 	}
 
@@ -227,18 +236,19 @@ public class NioClient implements Runnable {
 
 	public static void main(String[] args) {
 		try {
-			NioClient client = new NioClient(InetAddress.getByName("localhost"), 9090);
+			NioClient client = new NioClient(
+					InetAddress.getByName("localhost"), 9090);
 			Thread t = new Thread(client);
 			t.setDaemon(true);
 			t.start();
 			RspHandler handler = new RspHandler();
 			System.out.println("Client Running....");
-			
-			BufferedReader keyboard = new BufferedReader(
-					new InputStreamReader(System.in));
+
+			BufferedReader keyboard = new BufferedReader(new InputStreamReader(
+					System.in));
 			System.out.print("client -> string to send : ");
 			String strSend = keyboard.readLine();
-			
+
 			client.send(strSend.getBytes(), handler);
 			handler.waitForResponse();
 		} catch (Exception e) {

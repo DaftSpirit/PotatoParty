14914e35a86b7ffe7f9ebf245b8e29a637e61445
Karuskrokro <karuskrokro@hotmail.fr>
2015-12-30 15:36:42 +0100

    wololo

---
+++ PotatoParty/src/pp/nio/Protocol.java
@@ -0,0 +1,36 @@
+package pp.nio;
+
+public class Protocol {
+		
+	
+	public static final int GAME_OVER = 0 ;
+
+	public static final int WIN = 10;
+	
+
+	public static final int INIT = 20;
+	
+	public static final int INIT_OK = 21;
+
+	
+	public static final int HAUT = 30;
+	
+	public static final int HAUT_OK = 31;
+
+	
+	public static final int BAS = 40;
+	
+	public static final int BAS_OK = 41;
+
+	
+	public static final int GAUCHE = 50;
+	
+	public static final int GAUCHE_OK = 51;
+
+
+	public static final int DROITE = 60;
+	
+	public static final int DROITE_OK = 61;
+
+
+}
--- PotatoParty/src/pp/nio/RspHandler.java
+++ PotatoParty/src/pp/nio/RspHandler.java
@@ -16,7 +16,50 @@ public class RspHandler {
 			} catch (InterruptedException e) {
 			}
 		}
-
-		System.out.println(new String(this.rsp));
+		
+		/* TRAITEMENT DE LA REPONSE DU SERVEUR */
+		
+		String str = new String(this.rsp);
+		int res = Integer.valueOf(str);
+		System.out.println(res);
+		//String cmd = res.replaceAll("[0-9]", ""); Pas besoin de regex ?
+		switch(res) {
+		case Protocol.GAME_OVER:
+			System.out.println("You've Lost");
+			this.gameover();
+			break;
+		
+		case Protocol.WIN:
+			System.out.println("You've Win");
+			this.win();
+			break;
+			
+		case Protocol.INIT_OK:
+			System.out.println("Initialized..");
+			this.init();
+			break;
+	
+		case Protocol.HAUT_OK:
+			System.out.println("Pressed Up");
+			this.up();
+			break;
+			
+		case Protocol.BAS_OK:
+			System.out.println("Pressed Down");
+			this.down();
+			break;
+			
+		case Protocol.GAUCHE_OK:
+			System.out.println("Pressed Left");
+			this.left();
+			break;
+			
+		case Protocol.DROITE_OK:
+			System.out.println("Pressed Right");
+			this.right();
+			break;
+			
+			
+		}
 	}
 }
